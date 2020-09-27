ed260ae2a9b73dceb5d63901f10d00f156bfcfff
Alex <a80nieves@gmail.com>
2020-09-27 17:07:58 -0400

    added serve static files with templates

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("./templates/*"))
+	tpl = template.Must(template.ParseGlob("./templates/*"))	
 }
 
-func main() {
+func main() {	
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", index)
 	fmt.Println("Listening on port 3000")
 	http.ListenAndServe(":3000", nil)
