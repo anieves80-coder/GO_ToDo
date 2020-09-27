3b717add369ca2b1ec3016bc5f0717e3a22de24a
Alex <a80nieves@gmail.com>
2020-09-27 11:55:35 -0400

    server serves index template

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("Started")
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("./templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	fmt.Println("Listening on port 3000")
+	http.ListenAndServe(":3000", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	u := "test"
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
 }
