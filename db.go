c23a5ee972dd40525fe7a84cb4026d310373df9c
Alex <a80nieves@gmail.com>
2020-10-03 14:22:55 -0400

    formated code

--- db.go
+++ db.go
@@ -1,36 +1,35 @@
 package main
 
-
 import (
-    "context"
-	"fmt"   
-	"log" 
+	"context"
+	"fmt"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var collection *mongo.Collection	
+var collection *mongo.Collection
 
 //DBres is a struct for the data returned when searching the database
 type DBres struct {
-	ID primitive.ObjectID `bson:"_id,omitempty"`
-	StringID string	`json:"id"`
-    Date  string `json:"date"`
-    Description string `json:"description"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	StringID    string             `json:"id"`
+	Date        string             `json:"date"`
+	Description string             `json:"description"`
 }
 
-func init(){
-	
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")	
+func init() {
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.TODO(), clientOptions) //context.TODO() = returns an empty context
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	// Tests to see if there is a proper connection to the mongodb server
 	err = client.Ping(context.TODO(), nil)
 
@@ -39,34 +38,34 @@ func init(){
 	}
 
 	fmt.Println("Connected to MongoDB!")
-	collection = client.Database("gotodo").Collection("todolist")		
+	collection = client.Database("gotodo").Collection("todolist")
 }
 
-func insertDB(data Data) string {	
+func insertDB(data Data) string {
 	_, err := collection.InsertOne(context.TODO(), data)
-		if err != nil {
-			return "err"
-		}
+	if err != nil {
+		return "err"
+	}
 	return "ok"
 }
 
 func getAll() []DBres {
-	
+
 	var result DBres
 	var results []DBres
-	
+
 	res, err := collection.Find(context.TODO(), bson.D{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for res.Next(context.Background()) {	
-		err := res.Decode(&result)		
-		if err != nil { 
+	for res.Next(context.Background()) {
+		err := res.Decode(&result)
+		if err != nil {
 			log.Fatal(err)
-		}		
+		}
 		result.StringID = result.ID.Hex()
-		results = append(results, result)		  
+		results = append(results, result)
 	}
 	res.Close(context.TODO())
 
--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
-	"net/http"
 	"io/ioutil"
-	"encoding/json"
+	"net/http"
 )
 
 var tpl *template.Template
 
 //Data struct for data coming in from the clients ajax request
-type Data struct {    
-	Date string `json:"date"`
+type Data struct {
+	Date        string `json:"date"`
 	Description string `json:"info"`
 }
 
 func init() {
-	tpl = template.Must(template.ParseGlob("./templates/*"))	
+	tpl = template.Must(template.ParseGlob("./templates/*"))
 }
 
-func main() {	
+func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	http.HandleFunc("/", index)
 	http.HandleFunc("/addRec", addRec)
-	
+
 	fmt.Println("Listening on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	r := getAll()	
+	r := getAll()
 	tpl.ExecuteTemplate(w, "index.gohtml", r)
 }
 
 func addRec(w http.ResponseWriter, req *http.Request) {
-	var getInfo Data	
-	body, _ := ioutil.ReadAll(req.Body)//reads a buffer and returns a byte array	
-	json.Unmarshal(body, &getInfo)//Parses the JSON to the struct getInfo
-	m := insertDB(getInfo)	
-	mMap := map[string]string{"msg":m}
-	
-	m2,_ := json.Marshal(mMap)
-	if m != "err"{
+	var getInfo Data
+	body, _ := ioutil.ReadAll(req.Body) //reads a buffer and returns a byte array
+	json.Unmarshal(body, &getInfo)      //Parses the JSON to the struct getInfo
+	m := insertDB(getInfo)
+	mMap := map[string]string{"msg": m}
+
+	m2, _ := json.Marshal(mMap)
+	if m != "err" {
 		fmt.Fprintln(w, string(m2))
 	}
 }
